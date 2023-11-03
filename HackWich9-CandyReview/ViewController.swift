76bf0f7535fcf6d53ac5fd98978e4913edce3c88
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-11-02 20:50:55 -1000

    Initial Commit

---
+++ HackWich9-CandyReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackWich9-CandyReview
+//
+//  Created by Kapena Kaaihue on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
