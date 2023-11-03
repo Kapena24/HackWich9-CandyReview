04022943eea7c01eb033959d62c66f7930321a93
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-11-02 23:17:14 -1000

    I wasnt using my  time correctly so far🥲

--- HackWich9-CandyReview/ViewController.swift
+++ HackWich9-CandyReview/ViewController.swift
@@ -7,15 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func learnMorePress(_ sender: UIButton) {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
     
+  
+    var candyArray  = ["Reese's PeanutButter Cup", "Kit Kat", "Butterfingers", "Chotolate & Nut M & M"]
+    var ratingArray = ["⭐️⭐️⭐️⭐️⭐️", "⭐️⭐️⭐️", "⭐️⭐️⭐️⭐️", "⭐️⭐️⭐️⭐️⭐️"]
+    
+    var categoryOneImagesData = [String]()
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return candyArray.count
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
---
+++ HackWich9-CandyReview/secondDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  secondDetailViewController.swift
+//  HackWich9-CandyReview
+//
+//  Created by Kapena Kaaihue on 11/2/23.
+//
+
+import UIKit
+
+class secondDetailViewController: UIViewController{
+    
+    var imagePass: String?
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    
+    }
+
+    
+}
